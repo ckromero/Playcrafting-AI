0a69b1d3bc2b7e42f828df3521b37f50bd5c015b
Porrith Suong <psuong95@gmail.com>
2016-08-07 19:48:42 -0400

    Adds a Shootable Layer and a Health script to the repo.

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour
+{
+
+	public float maxHealth = 100f;
+
+	private float currentHealth;
+
+	private void Awake ()
+	{
+		currentHealth = maxHealth;
+	}
+
+	public void Damage (float amount)
+	{
+		if (amount <= 0f) {
+			return;
+		}
+
+		currentHealth -= amount;
+
+		if (currentHealth <= 0f) {
+			Die ();	
+		}
+	}
+
+	public virtual void Die ()
+	{
+		Destroy (gameObject);
+	}
+}

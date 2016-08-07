99a6a28f1d19e147c76d8f53f15ef539e398b260
Porrith Suong <psuong95@gmail.com>
2016-08-07 17:35:41 -0400

    Adds a CameraController to the repo.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+	public Transform target;
+	[Range (0f, 10f)]
+	public float cameraDampening = 5f;
+
+	private Vector3 cameraOffset;
+
+	// Use this for initialization
+	private void Start ()
+	{
+		if (target == null) {
+			target = GameObject.FindGameObjectWithTag ("Player").transform;
+		}
+		cameraOffset = transform.position - target.position;
+	}
+	
+	// Update is called once per frame
+	private void Update ()
+	{
+		var cameraPosition = target.position + cameraOffset;
+
+		transform.position = Vector3.Lerp (transform.position, cameraPosition, cameraDampening * Time.deltaTime);
+	}
+}

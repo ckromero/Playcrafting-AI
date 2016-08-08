4c65437c3131c0e623b70516ffa4a05a6654d3d3
Porrith Suong <psuong95@gmail.com>
2016-08-08 11:55:13 -0400

    Adds comments to the health.

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -4,15 +4,25 @@ using System.Collections;
 public class Health : MonoBehaviour
 {
 
+	/// <summary>
+	/// Amount of health we want to give to this character
+	/// </summary>
 	public float maxHealth = 100f;
 
+	/// <summary>
+	/// Internal value of the health
+	/// </summary>
 	private float currentHealth;
 
-	protected virtual void Awake ()
+	protected virtual void Start ()
 	{
 		currentHealth = maxHealth;
 	}
 
+	/// <summary>
+	/// Applies a damage value to the health.
+	/// </summary>
+	/// <param name="amount">Amount of damage done to the health</param>
 	public void Damage (float amount)
 	{
 		if (amount <= 0f) {
@@ -26,6 +36,9 @@ public class Health : MonoBehaviour
 		}
 	}
 
+	/// <summary>
+	/// Action to execute when the health reaches below 0.
+	/// </summary>
 	public virtual void Die ()
 	{
 		Destroy (gameObject);

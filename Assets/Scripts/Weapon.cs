d5ed9d93ab7db39c20cd558b626e72ee9fbd30d0
Porrith Suong <psuong95@gmail.com>
2016-08-08 12:16:31 -0400

    Adds a Weapon script for the AI component.'

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour
+{
+	[Range (0f, 100f)]
+	public float damage = 34f;
+
+	private Collider weaponCollider;
+
+	// Use this for initialization
+	private void Start ()
+	{
+		weaponCollider = GetComponent<Collider> ();
+		weaponCollider.isTrigger = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	private void OnTriggerEnter (Collider other)
+	{
+		Debug.Log (other.name);
+		if (other.CompareTag ("Player")) {
+			var health = other.GetComponent<Health> ();
+
+			if (health != null) {
+				health.Damage (damage);
+			}
+		}
+	}
+}

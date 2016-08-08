5a293c0c07c8e4ca6be15e5d79cb9d8f9fa72f1e
jaysuong <jaysuong@gmail.com>
2016-08-08 11:37:38 -0400

    Added AIBrain and IAIBehavior

---
+++ Assets/Scripts/State Machine AI/IAIBehavior.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Simple interface to describe an AI Behavior
+/// </summary>
+public interface IAIBehavior {
+
+    string name { get; }
+
+    void Stop();
+}

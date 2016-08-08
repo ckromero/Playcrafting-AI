2f29792bdb60b9c809028161f08df5394b480a52
jaysuong <jaysuong@gmail.com>
2016-08-08 11:59:20 -0400

    Added IAIBrain and AIBehavior

---
+++ Assets/Scripts/State Machine AI/IAIBrain.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IAIBrain {
+
+    bool SetBehaviorAsActive(IAIBehavior behavior);
+    bool IsBehaviorActive(IAIBehavior behavior);
+}

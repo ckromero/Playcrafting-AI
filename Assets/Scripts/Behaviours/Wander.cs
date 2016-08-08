ff383da7ee2da456b702120f91911322ae0ae561
Porrith Suong <psuong95@gmail.com>
2016-08-08 12:52:15 -0400

    Adds monobehavior related calls in IAIBrain.

---
+++ Assets/Scripts/Behaviours/Wander.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wander : AIBehavior
+{
+	private NavMeshAgent navAgent;
+
+	protected override void OnBehaviorStart ()
+	{
+		if (navAgent == null) {
+		}
+	}
+}
--- Assets/Scripts/State Machine AI/IAIBrain.cs
+++ Assets/Scripts/State Machine AI/IAIBrain.cs
@@ -1,18 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public interface IAIBrain {
+public interface IAIBrain
+{
 
-    /// <summary>
-    /// Attempts to set the behavior as an active behavior.  Check fails if the behavior is 
-    /// already active.
-    /// </summary>
-    /// <param name="behavior">The behavior to set as active</param>
-    /// <returns>False if it is already active</returns>
-    bool SetBehaviorAsActive(IAIBehavior behavior);
+	/// <summary>
+	/// Attempts to set the behavior as an active behavior.  Check fails if the behavior is 
+	/// already active.
+	/// </summary>
+	/// <param name="behavior">The behavior to set as active</param>
+	/// <returns>False if it is already active</returns>
+	bool SetBehaviorAsActive (IAIBehavior behavior);
 
-    /// <summary>
-    /// Checks to see if the current behavior is in the active set.
-    /// </summary>
-    bool IsBehaviorActive(IAIBehavior behavior);
+	/// <summary>
+	/// Checks to see if the current behavior is in the active set.
+	/// </summary>
+	bool IsBehaviorActive (IAIBehavior behavior);
+
+	#region Monobehavior Calls
+
+	/// <summary>
+	/// Gets the transform.
+	/// </summary>
+	/// <value>The transform.</value>
+	Transform transform { get; }
+
+	/// <summary>
+	/// Gets the game object.
+	/// </summary>
+	/// <value>The game object.</value>
+	GameObject gameObject { get; }
+
+	/// <summary>
+	/// Gets the component.
+	/// </summary>
+	/// <returns>The component.</returns>
+	/// <typeparam name="T">The 1st type parameter.</typeparam>
+	T GetComponent<T> ();
+
+	/// <summary>
+	/// Gets the first component in children.
+	/// </summary>
+	/// <returns>The component in children.</returns>
+	/// <typeparam name="T">The 1st type parameter.</typeparam>
+	T GetComponentInChildren<T> ();
+
+	/// <summary>
+	/// Gets the components in children.
+	/// </summary>
+	/// <returns>The components in children.</returns>
+	/// <typeparam name="T">The 1st type parameter.</typeparam>
+	T[] GetComponentsInChildren<T> ();
+
+	#endregion
 }
